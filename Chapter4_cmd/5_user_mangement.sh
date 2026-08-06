944b0c974191aa76d9957602c83ec028940824dc
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-08-05 21:51:21 -0400

    user

--- Chapter4_cmd/5_user_mangement.sh
+++ Chapter4_cmd/5_user_mangement.sh
@@ -2,8 +2,10 @@ sudo useradd liming
 sudo passwd liming 
 
 
+用户修改自己的密码 
+passwd # No need add username ,  因为普通用户改密码就是改自己这个用户的密码
 # shenchao sh@mcha0  passwd 
 
-who
-w          # up: uptime 
-whoami
+who        # 当前登录用户
+w          # u一切用户， 其中up: uptime 
+whoami     # 我/当前用户是谁
