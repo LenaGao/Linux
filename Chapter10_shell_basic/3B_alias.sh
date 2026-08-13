491ee6138e838bdf9968ba5fce26c9c68dcf53af
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-08-12 20:50:33 -0400

    shortcut key

---
+++ Chapter10_shell_basic/3B_alias.sh
@@ -0,0 +1,19 @@
+alias # 显示本系统别名
+
+命令执行顺序
+
+1.以绝对路径或相对路径执行的命令
+2.别名
+3.系统内部命令 
+4.$PATH 环境变量定义的目录顺序找到的第一个命令
+
+unalias
+
+快捷键 shortcut
+
+Ctrl + C # stop
+Ctrl + L # clear
+Ctrl + U # delete all earlier characteristics
+Ctrl + Y # paste
+Ctrl + D # logout
+Ctrl + Z # bg work
