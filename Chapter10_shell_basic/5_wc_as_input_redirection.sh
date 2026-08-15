b82dbb45be10b93396a58b918174fbf1488b604d
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-08-14 21:33:19 -0400

    redirection

--- Chapter10_shell_basic/5_wc_as_input_redirection.sh
+++ Chapter10_shell_basic/5_wc_as_input_redirection.sh
@@ -4,4 +4,18 @@ wc /etc/passwd
 wc -l /etc/passwd
 wc -w /etc/passwd
 wc -m /etc/passwd
+
+输入重定向 : < 
+wc <  date
+
+<< string  string  两个符号之间的内容作为输出
+# 这里字符串的实例是 st
+wc << st
+dadda
+st
+
+
+
 df -h | wc -l 
+
+
