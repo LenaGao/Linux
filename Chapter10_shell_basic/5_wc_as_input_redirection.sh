3c660e4f8d7077f59128f8092b165173fe22a4ce
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-08-16 21:53:50 -0400

    input redirection < and <<

--- Chapter10_shell_basic/5_wc_as_input_redirection.sh
+++ Chapter10_shell_basic/5_wc_as_input_redirection.sh
@@ -5,10 +5,13 @@ wc -l /etc/passwd
 wc -w /etc/passwd
 wc -m /etc/passwd
 
+
 输入重定向 : < 
 wc <  date
 
-<< string  string  两个符号之间的内容作为输出
+输入重定向之 "<<" , 更罕见，这里作为知识介绍:  
+
+<< string  string  两个字符串(string)之间的内容作为输出
 # 这里字符串的实例是 st, st 前后各出现一次，之间的内容才是真正的输入
 wc << st
 dadda
