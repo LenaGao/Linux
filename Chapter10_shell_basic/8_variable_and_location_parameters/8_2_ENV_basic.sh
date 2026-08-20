d7f493731e1c711a2ffdd3946069818371f02cec
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-08-20 17:28:54 -0400

    ENV variables

--- Chapter10_shell_basic/8_variable_and_location_parameters/8_2_ENV_basic.sh
+++ Chapter10_shell_basic/8_variable_and_location_parameters/8_2_ENV_basic.sh
@@ -1,6 +1,12 @@
+# 用户自定义变量的命名，作用都自由
+# 用户自定义只在当前shell中生效
+
 name=sc
 age=18
 
+# 环境变量允许用户添加，或者把某些变量声明为环境变量
+# 环境变量会在当前shell和这个shell的一切子shell中生效
+# 环境变量如果写入配置文件，那么就会在一切shell中生效
 
 export age 
 export gender=male 
