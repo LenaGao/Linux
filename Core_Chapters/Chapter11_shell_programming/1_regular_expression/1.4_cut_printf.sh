74c32a26768226d07fd30dee9c4424e92b06fa67
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-03 16:39:45 -0400

    summary

--- Core_Chapters/Chapter11_shell_programming/1_regular_expression/1.4_cut_printf.sh
+++ Core_Chapters/Chapter11_shell_programming/1_regular_expression/1.4_cut_printf.sh
@@ -1,5 +1,10 @@
-cut 字段截取命令
-printf 格式化输出命令
+grep 字符串搜索命令, 提取符合条件的行 
 
+cut 和 awk 提取符合条件的列
 这两个命令的更多实例在
 /home/lena/Linux/Core_Chapters/Chapter11_shell_programming/2_column_extract_cut_and_printf
+
+cut 字段截取命令
+printf 格式化输出命令
+awk 提取符合条件的列，类似于cut命令，但是功能更强大，常说awk编程
+sed 流编辑器，类似于vi编辑器，但是功能更强大，常说sed编程
