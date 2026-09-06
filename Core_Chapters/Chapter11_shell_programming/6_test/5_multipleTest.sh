78e39e7daedccf2b0aaddd142be0cb62db9d4539
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-05 21:50:08 -0400

    test format 和 if 格式

--- Core_Chapters/Chapter11_shell_programming/6_test/5_multipleTest.sh
+++ Core_Chapters/Chapter11_shell_programming/6_test/5_multipleTest.sh
@@ -1,9 +1,24 @@
 #!/bon/bash
 v=11
 
-# -a 
-# -o
+AND 
+-a  
 
-[ -n $v -a $v -gt 23 ] && echo "both tests are true" || echo "not all tests are true"
+OR  
+-o
 
+not
+!
+
+[ -n $v   -a $v -gt 23 ]   && echo "both tests are true" || echo "not all tests are true"
+
+[ -n $v ] && echo "both tests are true" || echo "not all tests are true"
+[ $v -gt 23 ] && echo "both tests are true" || echo "not all tests are true"
+
+
+
+v=24
+
+[ -n $v   -a  $v -gt 23 ] && echo "both tests are true" || echo "not all tests are true"
 [ -n "$v" -a "$v" -gt 23 ] && echo "both tests are true" || echo "not all tests are true"
+echo $v
--- Core_Chapters/Chapter11_shell_programming/7_if_check_backup/1_test_shell_type.sh
+++ Core_Chapters/Chapter11_shell_programming/7_if_check_backup/1_test_shell_type.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+
+
+# test if the directory /root exists
+使用专用的test格式
+[ -e /root ] && echo "the directory /root exists" || echo "the directory /root does not exist" 
+
+
+# test if 我的bash 是 /bin/bash
+使用常见的if then else 格式 
+
 if [ "$SHELL" = "/bin/bash" ]; then
-echo "your login shell is the bash (bourne again shell)"
+    echo "your login shell is the bash (bourne again shell)"
 else
-echo "your login shell is not bash but $SHELL"
+    echo "your login shell is not bash but $SHELL"
 fi
 
 # if ; then  在一行中需要加； 
