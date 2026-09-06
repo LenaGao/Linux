8d7f92f26e7b547c46ed4dc65ad80f9708687391
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-05 21:22:20 -0400

    number_test

--- Core_Chapters/Chapter11_shell_programming/6_test/3_number_comparsion.sh
+++ Core_Chapters/Chapter11_shell_programming/6_test/3_number_comparsion.sh
@@ -1,8 +1,15 @@
 #!/bon/bash
+
+数值比对
+
+大于等于 greater than or equal
 [ 30 -ge 30 ] && echo "yes, it's larger or egual" || echo no
 
+大于
 # [ 30 -gt 30 ]
+不相等
 # [ 30 -ne 30 ]
+xiao 
 # [ 30 -lt 30 ]
 # [ 30 -le 30 ]
 
