0abfd7b81d8cf1c1ff35c174495bae340c559b8d
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-05 21:58:30 -0400

    usage test

--- Core_Chapters/Chapter11_shell_programming/7_if_check_backup/2_measure_rate.sh
+++ Core_Chapters/Chapter11_shell_programming/7_if_check_backup/2_measure_rate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#Author Zhen Gao (Email: zhen.gao@yahoo.com)
 
-rate=$(df -h| grep "/dev/sda" | awk '{print $5}'| cut -d "%" -f1)
+上面这句话shibang指定了这个脚本的解释器是bash shell，注意不是sh shell
+上面这句话不要省
+
+#Author Zhen Gao (Email: zhen.gao@yahoo.com) # 注释
+
+rate=$(df -h| grep "/dev/sd" | awk '{print $5}'| cut -d "%" -f1)
 #get rate of using
 
 echo $rate
