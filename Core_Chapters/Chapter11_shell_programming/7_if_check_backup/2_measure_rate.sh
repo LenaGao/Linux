5aa6f47e3f7735a8f4c2dfa2e6d0c904c19eff67
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-06 19:22:32 -0400

    disk usage

--- Core_Chapters/Chapter11_shell_programming/7_if_check_backup/2_measure_rate.sh
+++ Core_Chapters/Chapter11_shell_programming/7_if_check_backup/2_measure_rate.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-上面这句话shibang指定了这个脚本的解释器是bash shell，注意不是sh shell
-上面这句话不要省
+上面这句话shibang指定了这个脚本的解释器是bash shell，注意不是sh shell, 
+bash shell是sh shell的一个增强版本，支持更多的功能和特性。
+上面这句话不要省,
+各种语言的脚步基本上都可以看见第一行是这一句
 
 #Author Zhen Gao (Email: zhen.gao@yahoo.com) # 注释
 
 rate=$(df -h| grep "/dev/sd" | awk '{print $5}'| cut -d "%" -f1)
-#get rate of using
+# get rate of using
+一定要去除百分号的原因是，
+bash 数值大小判断只能是纯数字 
 
 echo $rate
 if [ $rate -lt 20 ]
-then
-    echo "The system is healthy"
-else
-    echo "The disk is nearly fully, please take care"
+    then
+        echo "The system is healthy"
+    else
+        echo "The disk is nearly fully, please take care"
 fi
