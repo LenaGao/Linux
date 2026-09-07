02d9878189e38a189680f6b08c20ddd8d3c334d7
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-06 20:12:09 -0400

    backup3

--- Core_Chapters/Chapter11_shell_programming/7_if_check_backup/3_backup.sh
+++ Core_Chapters/Chapter11_shell_programming/7_if_check_backup/3_backup.sh
@@ -7,7 +7,7 @@
 # 260906
 
 # date=$(date +\%y\%m\%d)  # contab %有特殊含义，不能直接使用，需要用转义符\转义
-date= $(date +%y%m%d) 
+date=$(date +%y%m%d)  # 2026注，没看到contab的问题， 这个命令可以直接运行
 size=$(sudo du -sh /etc) # 获取 /etc 目录的大小（sudo 因为部分文件普通用户无权限读取）
 
 echo $date
@@ -15,16 +15,14 @@ echo $size
 
 # 判断备份目录 /tmp/dbback 是否已存在
 if [ -d /tmp/dbback ] # 判断备份目录存在与否
-    then
-        # 目录已存在：直接在其中生成记录文件并打包
+    then              # 目录已存在：直接在其中生成记录文件并打包
         echo "Date is $date" > /tmp/dbback/db.txt      # 记录备份日期
-        echo "size is $size" >>/tmp/dbback/db.txt      # 记录 /etc 目录大小
+        echo "size is $size" >>/tmp/dbback/db.txt      # 记录 /etc 目录大小, 追加到同一个文件
         cd /tmp/dbback
-        tar -zcf etc_$date.tar.gz /etc db.txt &>/dev/null        # 把/etc 和上面刚生成的db.txt 一起进行压缩
+        tar -zcf etc_$date.tar.gz /etc db.txt &>/dev/null        # 把/etc 和上面刚生成的db.txt ， 两个文件， 一起进行压缩
         #tar -zcf mysql-lib_$date.tar.gz /var/lib/mysql  &>/dev/null
         rm -rf /tmp/dbback/db.txt                      # 打包完成后删除临时的记录文件
-    else
-        # 目录不存在：先创建目录，再执行与上面相同的备份流程
+    else              # 目录不存在：先创建目录，再执行与上面相同的备份流程
         mkdir /tmp/dbback
         echo "Date is $date" > /tmp/dbback/db.txt
         echo "size is $size" >>/tmp/dbback/db.txt
