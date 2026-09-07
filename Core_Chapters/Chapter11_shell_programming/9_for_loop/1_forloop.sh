4bcecadd29b796d0b50943b754725a2bb4a9b451
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-07 11:39:41 -0400

    forloop

--- Core_Chapters/Chapter11_shell_programming/9_for_loop/1_forloop.sh
+++ Core_Chapters/Chapter11_shell_programming/9_for_loop/1_forloop.sh
@@ -1,4 +1,4 @@
-
+有空格就作为一项
 for var in A B C ; do
 echo "var is $var"
 done
--- Core_Chapters/Chapter11_shell_programming/9_for_loop/2_for1.sh
+++ Core_Chapters/Chapter11_shell_programming/9_for_loop/2_for1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 有空格就作为一项
 for day in Monday Tuesday Wednesday Thursday Friday Saturday Sunday
     do
        echo $day
