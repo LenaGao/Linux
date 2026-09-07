be733e66dbc343b30c12684940a06ecdb8c09eb8
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-07 19:20:10 -0400

    until

--- Core_Chapters/Chapter11_shell_programming/10_while_and_until/4_until.sh
+++ Core_Chapters/Chapter11_shell_programming/10_while_and_until/4_until.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# 和while相反，条件不成立的时候则一直循环
 i=1
-until [ $i -gt 6 ]
+
+until [ $i -gt 6 ]   # 条件和whil 相反， while 是小于等于6的时候 一直运行， 这里是在大于等于6的时候，停止循环
 do
 	echo "Welcome $i times."
 	i=$(( i+1 ))
