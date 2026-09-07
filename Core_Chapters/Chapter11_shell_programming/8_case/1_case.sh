fe1ce2dade129a8d557274a5618a3e635f0beba2
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-06 21:37:32 -0400

    case + select

--- Core_Chapters/Chapter11_shell_programming/8_case/1_case.sh
+++ Core_Chapters/Chapter11_shell_programming/8_case/1_case.sh
@@ -1,8 +1,16 @@
 #!bin/bash
 
-echo 'If you want to buy ticket to Shanghai, please input "1"'
+# case 只能判断一种条件关系
+# if 可以多种和嵌套条件关系
+# 注意1： case 开头,  easc结尾 
+# 注意2： 每个选择读有 ;; ， in each case <=== 极其容易失误
+# 注意3： *) 用来包括其他一切不支持选项
+# 注意4： *) 没有引号
+
+
+echo 'If you want to buy ticket to Shanghai,  please input "1"'
 echo 'If you want to buy ticket to Guangzhou, please input "2"'
-echo 'If you want to buy ticket to Chengdu, please input "3"'
+echo 'If you want to buy ticket to Chengdu,   please input "3"'
 
 read -t 30 -p "Please input your choice: " cho
 
--- Core_Chapters/Chapter11_shell_programming/8_case/2_os_selection.sh
+++ Core_Chapters/Chapter11_shell_programming/8_case/2_os_selection.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "What is your favourite OS?"
+
+# 'select' prints a numbered menu of the given items and prompts for a choice;
+# the chosen item's text is stored in $var, the number typed is stored in $REPLY.
+# The loop repeats until 'break' (or a valid script-ending command) is executed.
+
 select var in "Linux" "Gnu Hurd" "Free BSD" "Other"; do
-break
+break   # exit the select loop right after the first choice is made
 done
-echo "You have selected $var"
+echo "You have selected $var"   # print back whichever option the user picked
