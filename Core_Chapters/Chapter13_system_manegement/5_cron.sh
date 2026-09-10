15039bc2d0f456069e09bdcd70181da049dd00dd
Lena_Gao in wsl <junyi-gao@hotmail.com>
2026-09-10 09:25:30 -0400

    Linux Cron

--- Core_Chapters/Chapter13_system_manegement/5_cron.sh
+++ Core_Chapters/Chapter13_system_manegement/5_cron.sh
@@ -1,5 +1,7 @@
-# (3) ============= cron 定时任务 ========================
-# cron是Linux系统中用于定时执行任务的守护进程
+# (1) ============= cron 定时任务 ========================
+
+cron是Linux系统中用于定时执行任务的守护进程
+
 # 定时任务配置文件位于 /etc/crontab 和 /etc/cron.d 目录下
 # 用户级别的定时任务配置文件位于 /var/spool/cron/crontabs 目录下
 # 系统级别的定时任务脚本存放在 /etc/cron.hourly/, /etc/cron.daily/, /etc/cron.weekly/, /etc/cron.monthly/ 目录下
@@ -11,7 +13,14 @@ systemctl start crond          # 启动cron服务
 
 crontab -l        # 查看当前用户的定时任务
 crontab -e        # 编辑当前用户的定时任务, 实际上是vim
-# crontab文件格式: 分 时 日 月 周      ==>> 命令
+# crontab文件格式: 
+分 时 日 月 周      ==>> 命令
+*/10  表示每隔10分钟
+
+45 22 * * * ls 2>&1 # 每天的22点45分执行ls 命令
+45 22 * * * ls 2>&1 # 每天的22点45分执行ls 命令
+
+
 # 示例: 每天凌晨2点执行备份脚本
 0 2 * * * /path/to/backup_script.sh >> /path/to/backup.log 2>&1
 
@@ -26,6 +35,9 @@ crontab -e        # 编辑当前用户的定时任务, 实际上是vim
 
 # 示例: 每天4点， 每格10分执行备份脚本
 */10 4  * * 1-5 /path/to/backup_script.sh >> /path/to/backup.log 2>&1
+
+
+
 
 # 以下最好不要同时出现两个时间限制条件，以免产生歧义，建议只使用其中一个，理论上是可以同时使用的
 # 示例: 每月1号和15号的凌晨0点执行备份脚本,每周一的凌晨0点执行备份脚本， 两者满足其一即可执行
--- Core_Chapters/Chapter13_system_manegement/6_cron_backup_ln.sh
+++ Core_Chapters/Chapter13_system_manegement/6_cron_backup_ln.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# 脚本功能：备份/etc 目录，并把备份文件放在 /tmp/dbback 目录下
-# hard lnk 硬链接  ln 源文件 目标文件
-# soft lnk 软链接 ln -s 源文件 目标文件
+
+这个脚本是cron 定时任务执行的对象， 比如每天的凌晨5：59分执行一次
+# 先赋予执行权限
+
+# chmod 0755 /home/lena/Linux/Core_Chapters/Chapter13_system_manegement/6_cron_backup_ln.sh
+
+# crontab -e
+# 然后在文件中写入
+# 0 5 *  *  * /home/lena/Linux/Core_Chapters/Chapter13_system_manegement/6_cron_backup_ln.sh
+
+
 #date=$(date +%y%m%d) # 获取当前日期，格式为 年月日，
-date=$(date +\%y\%m\%d) # contab %有特殊含义，不能直接使用，需要用转义符\转义     
+
+date=$(date +\%y\%m\%d)  # contab "%" 有特殊含义，不能直接使用，需要用转义符\转义     
 size=$(sudo du -sh /etc)
 
 if [ -d /tmp/dbback ]
@@ -23,3 +32,9 @@ if [ -d /tmp/dbback ]
         rm -rf /tmp/dbback/db.txt
 fi
 
+
+核心内容就是把进程放入后天
+
+# 脚本功能：备份/etc 目录，并把备份文件放在 /tmp/dbback 目录下
+# hard lnk 硬链接  ln 源文件 目标文件
+# soft lnk 软链接 ln -s 源文件 目标文件
